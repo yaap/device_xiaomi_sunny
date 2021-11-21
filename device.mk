8c19ec5165be4f275cad5889210d93cf378dc348
Tim Zimmermann <tim@linux4.de>
2021-11-21 20:17:51 +0100

    sunny: Uprev audio HAL to V7
    Updated audio policy by running:
    
    'hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/xiaomi/sunny/configs/audio/audio_policy_configuration.xml 7.0'
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -51,18 +51,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.common@2.0-util \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.common@6.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio.effect@5.0-impl \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     audio.usb.default \
     audio.r_submix.default \
