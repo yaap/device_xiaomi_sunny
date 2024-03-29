b415516cbf815bac37d07ad4f20c2a701a4d1da9
Cyber Knight <cyberknight755@gmail.com>
2024-03-30 07:42:02 +0800

    sunny: Drop `hardware/qcom-caf/wlan` from `PRODUCT_SOONG_NAMESPACES`
    - It is done in hardware/qcom-caf/common now.
    
    Change-Id: Iba08a0f1aa08b2b665ac3e6f35921363e25d0502
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -462,8 +462,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi \
-    hardware/qcom-caf/wlan
+    hardware/xiaomi
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
