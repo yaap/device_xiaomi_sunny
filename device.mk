b126c81acb940d90d328e58101aad5d4653c007a
Tim Zimmermann <tim@linux4.de>
2024-05-29 10:23:48 +0200

    sunny: Switch to AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -109,12 +109,10 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libdng_sdk.vendor \
     libcamera2ndk_vendor \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider-service.lineage
 
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    android.frameworks.displayservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
     camxoverridesettings.txt
