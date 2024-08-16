d23244e6d445e679df35cb77f1e4c8b7ef4b65c0
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:03:24 +0300

    sunny: Fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -10,6 +10,7 @@ from extract_utils.fixups_blob import (
 )
 from extract_utils.fixups_lib import (
     lib_fixups,
+    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -27,6 +28,16 @@ namespace_imports = [
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
 ]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    ('vendor.qti.imsrtpservice@3.0',): lib_fixup_vendor_suffix,
+}
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc': blob_fixup()
