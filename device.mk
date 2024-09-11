42933917e072c11cdd7de3bcdbf232c1d79fb21c
Cyber Knight <cyberknight755@gmail.com>
2024-09-12 00:56:26 +0800

    sunny: Move to sensors.xiaomi.v2
    - This is done in accordance to [1].
    
    [1]: https://github.com/yaap/hardware_xiaomi/commit/63fd908ef0174bbca27d7472bc4e8acad8018487
    
    Change-Id: Ia35bfd7db32f7511453c4c756f8d869c4c7b31c7
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -418,7 +418,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \
-    sensors.xiaomi
+    sensors.xiaomi.v2
 
 # Sensors configs
 PRODUCT_COPY_FILES += \
