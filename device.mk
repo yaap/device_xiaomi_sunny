63b89fd85f8c80f6880b0970aeec800dc01b15eb
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    sunny: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -178,9 +178,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
-    gralloc.default \
-    gralloc.sm6150 \
-    hwcomposer.sm6150
+    hwcomposer.qcom
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.0.vendor \
