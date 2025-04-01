ebc145c5048c59a8aea7d1fb22ed8907113c7c10
basamaryan <basam.aryan@gmail.com>
2025-04-01 21:24:09 +0200

    sunny: Drop in-tree FCM
    Change-Id: I7f10a5613a9192b70d08617927a2ab2e32da28bb
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,8 +82,7 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/configs/manifest/framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/manifest/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/manifest/manifest.xml
 
